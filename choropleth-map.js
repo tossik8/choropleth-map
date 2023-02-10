20c0a8bfd8c3bc54a307d6c4285443d67bfa1d4a
Nikita <toropov.nikita2004@gmail.com>
2023-02-10 23:30:29 +0100

    Render the map of the US

--- choropleth-map.js
+++ choropleth-map.js
@@ -1,18 +1,29 @@
 const width = 960;
 const height = 600;
+const margins = {top: 40, right: 40, bottom: 40, left: 40};
 
 d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
   .then(data => createMap(data));
 
 function createMap(data){
-    console.log(data);
-    d3.select(".panel")
+
+    const svg = d3.select(".panel")
       .append("svg")
       .attr("width", width)
       .attr("height", height);
-    d3.geoEqualEarth();
-    console.log(topojson.feature(data, data.objects.counties));
 
+    const map = svg.append("g");
+
+    const path = d3.geoPath();
+
+    const counties = topojson.feature(data, data.objects.counties);
+
+    map.selectAll(".states")
+       .data(counties.features)
+       .enter()
+       .append("path")
+       .attr("class", "county")
+       .attr("d", path);
 }
 
 // d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json")
