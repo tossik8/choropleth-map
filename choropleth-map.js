db21ccc433a82cad8901e09290b8b80c9ce53d14
Nikita <toropov.nikita2004@gmail.com>
2023-02-11 00:41:01 +0100

    Assign attributes to counties

--- choropleth-map.js
+++ choropleth-map.js
@@ -2,11 +2,12 @@ const width = 960;
 const height = 600;
 const margins = {top: 40, right: 40, bottom: 40, left: 40};
 
-d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
-  .then(data => createMap(data));
+const coordinates = d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json");
+const info = d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json");
 
-function createMap(data){
+Promise.all([coordinates, info]).then(values => createMap(values));
 
+function createMap(values){
     const svg = d3.select(".panel")
       .append("svg")
       .attr("width", width)
@@ -16,15 +17,27 @@ function createMap(data){
 
     const path = d3.geoPath();
 
-    const counties = topojson.feature(data, data.objects.counties);
+    const counties = topojson.feature(values[0], values[0].objects.counties);
 
-    map.selectAll(".states")
+    console.log(values[1])
+
+    map.selectAll(".county")
        .data(counties.features)
        .enter()
        .append("path")
        .attr("class", "county")
-       .attr("d", path);
+       .attr("d", path)
+       .attr("data-fips", d => d.id)
+       .attr("data-education", d => binarySearch(values[1], d.id));
 }
 
-// d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json")
-//     .then(data => data);
+function binarySearch(data, id){
+  let start = 0, end = data.length - 1;
+  while(start < end){
+    let index = Math.floor((start + end)/2);
+    if(data[index].fips === id) return data[index].bachelorsOrHigher;
+    if(data[index].fips > id) end = index - 1;
+    else start = index + 1;
+  }
+  return -1;
+}
