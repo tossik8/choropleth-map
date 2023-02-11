0d1816402b3d19b59da492c54c0a7dbbc18715af
Nikita <toropov.nikita2004@gmail.com>
2023-02-11 02:46:02 +0100

    Add colours and borders to the map

--- choropleth-map.js
+++ choropleth-map.js
@@ -8,6 +8,7 @@ const info = d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/ch
 Promise.all([coordinates, info]).then(values => createMap(values));
 
 function createMap(values){
+  console.log(values[0])
     const svg = d3.select(".panel")
       .append("svg")
       .attr("width", width)
@@ -25,15 +26,47 @@ function createMap(values){
        .data(counties.features)
        .enter()
        .append("path")
-       .attr("class", "county")
-       .attr("d", path)
        .attr("data-fips", d => d.id)
-       .attr("data-education", d => binarySearch(values[1], d.id));
+       .attr("data-education", d => binarySearch(values[1], d.id))
+       .attr("class", d=> `county ${assignColor(binarySearch(values[1], d.id))}`)
+       .attr("d", path);
+
+    const states = topojson.feature(values[0], values[0].objects.states);
+    map.selectAll(".state")
+       .data(states.features)
+       .enter()
+       .append("path")
+       .attr("class", "state")
+       .attr("d", path);
+}
+
+function assignColor(x){
+  if(x < 12) {
+    return "ultra-light-green";
+  }
+  else if(x < 21){
+    return "light-green";
+  }
+  else if(x < 30){
+    return "green";
+  }
+  else if(x < 39){
+    return "green-2";
+  }
+  else if(x < 48){
+    return "darker-green";
+  }
+  else if(x < 57){
+    return "darker-green-2";
+  }
+  else{
+    return "dark-green";
+  }
 }
 
 function binarySearch(data, id){
   let start = 0, end = data.length - 1;
-  while(start < end){
+  while(start <= end){
     let index = Math.floor((start + end)/2);
     if(data[index].fips === id) return data[index].bachelorsOrHigher;
     if(data[index].fips > id) end = index - 1;
